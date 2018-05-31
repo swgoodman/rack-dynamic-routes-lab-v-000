349daac9d07a4140dec0951aed72ab3531360760
Spencer Goodman <spencerwgoodman@icloud.com>
2018-05-31 01:35:07 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -4,5 +4,10 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-
+    if req.path =="/songs"
+    else
+      resp.write "Route not found"
+      resp.status "404"
+    end
+    resp.finish
 end
