df160da0426b322761daf83c12e7b191d8797e1e
Spencer Goodman <spencerwgoodman@icloud.com>
2018-05-31 01:36:13 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -4,7 +4,7 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    if req.path =="/items"
+    if req.path =="/items/"
     else
       resp.write "Route not found"
       resp.status "404"
