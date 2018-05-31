4224e2a12492733a21bc20def2b9fceb5d3a433a
Spencer Goodman <spencerwgoodman@icloud.com>
2018-05-31 01:53:27 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -8,7 +8,7 @@ class Application
 
     if req.path.match(/items/)
       if @@items.include?(req.path[7..-1])
-        selected_item = @@items.find {|i| i.name == req.path[7..-1]}
+        selected_item = @@items.find {|i| i.name == "req.path[7..-1]"}
         resp.write selected_item.price
       else
         resp.write "Item not found"
