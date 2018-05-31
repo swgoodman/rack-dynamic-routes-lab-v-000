6396aa078971a7da0422d4186c96bc3238928c59
Spencer Goodman <spencerwgoodman@icloud.com>
2018-05-31 01:54:50 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -7,7 +7,8 @@ class Application
     req = Rack::Request.new(env)
 
     if req.path.match(/items/)
-      if @@items.include?(req.path[7..-1])
+      selected_item = @@items.find {|i| i.name == req.path[7..-1]}
+      if @@items.include?(selected_item)
         resp.write "This is working"
       else
         resp.write "Item not found"
