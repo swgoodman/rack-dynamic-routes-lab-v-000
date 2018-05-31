172b5da9dd21811523739b0784de7082b4517597
Spencer Goodman <spencerwgoodman@icloud.com>
2018-05-31 01:47:59 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -6,7 +6,7 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    if req.path =="/items/"
+    if req.path.match(/items/)
       if @@item.contain?(req.path[7..-1])
         resp.write "This is working"
       else
